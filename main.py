1455d68dcbbad83bbb3935d691304e4266d13698
jesper-sk <jesper.kuiper99@gmail.com>
2022-04-29 13:31:16 +0200

    Update pipeline

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import argparse
 
 
-def main():
-    print("Hello, world!")
+def dss(args):
+    pass
+
+
+def iam(args):
+    pass
+
+
+def parse_args():
+    # Create top-level parser
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='the task to scope to: dss=Dead Sea Scrolls tasks; iam=IAM-dataset task')
+
+    # Create parser for Dead Sea Scrolls task
+    parser_dss = subparsers.add_parser('dss')
+    # ...arguments
+    parser_dss.set_defaults(func=dss)
+
+    # Create parses for IAM task
+    parser_iam = subparsers.add_parser('iam')
+    # ...arguments
+    parser_iam.set_defaults(func=iam)
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    args.func(args)
--- utils/font2image.py
+++ utils/font2image.py
@@ -2,10 +2,10 @@
 from PIL import Image, ImageFont, ImageDraw
 
 # Load the font and set the font size to 42
-font = ImageFont.truetype('Habbakuk.ttf', 42)
+font = ImageFont.truetype('../data/dss/font/Habbakuk.TTF', 42)
 
 # Character mapping for each of the 27 tokens
-char_map = {'Alef': ')',
+CHAR_MAP = {'Alef': ')',
             'Ayin': '(',
             'Bet': 'b',
             'Dalet': 'd',
@@ -34,9 +34,14 @@ char_map = {'Alef': ')',
             'Zayin': 'z'}
 
 
-# Returns a grayscale image based on specified label of img_size
 def create_image(label, img_size):
-    if label not in char_map:
+    """Returns a grayscale image based on specified label of img_size
+
+    :param label: The label to create an image for
+    :param img_size: The size of the desired image
+    :return: An image of the specified size with specified character
+    """
+    if label not in CHAR_MAP:
         raise KeyError('Unknown label!')
 
     # Create blank image and create a draw interface
@@ -44,13 +49,17 @@ def create_image(label, img_size):
     draw = ImageDraw.Draw(img)
 
     # Get size of the font and draw the token in the center of the blank image
-    w, h = font.getsize(char_map[label])
-    draw.text(((img_size[0] - w) / 2, (img_size[1] - h) / 2), char_map[label], 0, font)
+    w, h = font.getsize(CHAR_MAP[label])
+    draw.text(((img_size[0] - w) / 2, (img_size[1] - h) / 2), CHAR_MAP[label], 0, font)
 
     return img
 
 
-# Create a 50x50 image of the Alef token and save it to disk
-# To get the raw data cast it to a numpy array
-img = create_image('Alef', (50, 50))
-img.save('example_alef.png')
+def example():
+    """Create a 50x50 image of the Alef token and save it to disk
+
+    To get the raw data cast it to a numpy array
+    """
+    img = create_image('Alef', (50, 50))
+    img.save('example_alef.png')
+
