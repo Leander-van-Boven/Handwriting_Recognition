89e74a65a7ae0e23042227390743a8b9e88de512
Leander <33727726+Saltpile123@users.noreply.github.com>
2022-05-22 14:43:46 +0200

    ngram updates

--- src/utils/ngrams_prepocessing.py
+++ src/utils/ngrams_prepocessing.py
@@ -8,12 +8,20 @@ import pandas as pd
 import numpy as np
 
 #%%
-# VARIABLES
-ngram_in_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_raw.csv'
-ngram_w_idx_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_w_idx.csv'
-ngram_out_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_processed.npy'
+# VARIABLES LEANDER DESKTOP
+# ngram_in_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_raw.csv'
+# ngram_w_idx_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_w_idx.csv'
+# ngram_out_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_processed.npy'
+#
+# character_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\characters'
 
-character_path = r'C:\Users\leand\Documents\_Studie\MSc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\characters'
+# VARIABLES LEANDER LAPTOP
+ngram_in_path = r'D:\Studie\Msc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_raw.csv'
+ngram_w_idx_path = r'D:\Studie\Msc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_w_idx.csv'
+ngram_out_path = r'D:\Studie\Msc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\ngrams\ngrams_processed.npy'
+
+character_path = r'D:\Studie\Msc\Jaar 2\4. HWR\Handwriting_Recognition\data\dss\characters'
+
 
 
 #%%
@@ -68,12 +76,58 @@ ngrams_pd['Names'] = ngrams_pd['Names'].apply(lambda ngram: '_'.join([ngram_char
 
 #%%
 # CONSTRUCT PROCESSED N-GRAMS
-# Consider every 2- to 10-gram combination (also consider n-grams with consecutive characters)
-ngrams_processed = pd.DataFrame(columns=['N', 'Names', 'Idx', 'Frequencies', 'Probability'])
-for n in ngrams:
-    for permutation in itertools.product(character_dirs, repeat=n):
-        ngram_str = '_'.join(permutation)
-        ngram_freq = ngrams_pd[ngrams_pd['Names'] == ngram_str]['Frequencies'].values[0] \
-            if ngram_str in ngrams_pd['Names'].values \
-            else 1  # If ngram is not in the dataset, set frequency to 1 (to account for 'unseen' n-grams)
-        ngrams[ngram_str] = ngram_freq
+ngrams_processed = ngrams_pd.copy()
+# label each n-gram with appropriate N
+ngrams_processed['N'] = ngrams_processed['Names'].apply(lambda ngram: len(ngram.split('_')))
+ngrams_processed = ngrams_processed.sort_values(by=['N', 'Frequencies', 'Names'], ascending=[True, False, True])
+
+#%%
+# CALCULATE PROBABILITY FOR EACH N-GRAM
+def calculate_ngram_probability_old(ngram_row):
+    ngram = ngram_row['Names']
+    n_minus_one_gram = '_'.join(ngram.split('_')[:-1])
+    if ngram_row['N'] == 2:
+        freq_n_minus_one_gram = \
+            (ngrams_processed[
+                (ngrams_processed['N'] == 2) &
+                ([name.startswith(n_minus_one_gram) for name in ngrams_processed['Names']])
+            ]['Frequencies']
+            ).sum()
+    else:
+        freq_n_minus_one_gram = ngrams_processed.apply(
+            lambda row: row['Frequencies']
+                if row['N'] == (ngram_row['N'] - 1) and row['Names'] == n_minus_one_gram
+                else 0,
+            axis=1
+        ).sum()
+
+    print(f'{ngram_row.name} ({ngram_row["N"]}-gram): {ngram_row["Frequencies"]} / {freq_n_minus_one_gram}')
+    return ngram_row['Frequencies'] / freq_n_minus_one_gram
+
+
+def calculate_ngram_probability(ngram_row):
+    ngram = ngram_row['Names']
+    # obtain the (n-1)-gram of the ngram we are dealing with
+    n_minus_one_gram = '_'.join(ngram.split('_')[:-1])
+    # calculate the frequency of the (n-1)-gram occuring in the corpus
+    # TODO: determine whether to use 'x in y' or 'y.startswith(x)'
+    freq_n_minus_one_gram = ngrams_processed.apply(
+        lambda row: row['Frequencies'] if row['Names'].startswith(n_minus_one_gram) else 0,
+        axis=1
+    ).sum()
+
+    # print(f'{ngram_row.name} ({ngram_row["N"]}-gram): {ngram_row["Frequencies"]} / {freq_n_minus_one_gram}')
+    return ngram_row['Frequencies'] / freq_n_minus_one_gram
+
+ngrams_processed['Probability'] = ngrams_processed.apply(calculate_ngram_probability, axis=1)
+
+# ngrams_processed = pd.DataFrame(columns=['N', 'Names', 'Idx', 'Frequencies', 'Probability'])
+# for n in ngrams:
+#     for permutation in itertools.product(character_dirs, repeat=n):
+#         ngram_str = '_'.join(permutation)
+#         ngram_freq = ngrams_pd[ngrams_pd['Names'] == ngram_str]['Frequencies'].values[0] \
+#             if ngram_str in ngrams_pd['Names'].values \
+#             else 1  # If ngram is not in the dataset, set frequency to 1 (to account for 'unseen' n-grams)
+#         ngrams[ngram_str] = ngram_freq
+
+#%%
