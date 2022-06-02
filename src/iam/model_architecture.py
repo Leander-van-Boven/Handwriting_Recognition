e07847f30a6d7ebaf3f73f4c61a3f7375ff75e4f
Leander-van-Boven <33727726+Leander-van-Boven@users.noreply.github.com>
2022-06-02 23:42:54 +0200

    added files for IAM

---
+++ src/iam/model_architecture.py
@@ -0,0 +1,11 @@
+import string
+
+from tensorflow.python.keras import models, layers, losses
+from tensorflow.python.keras.layers import PReLU, Softmax
+
+
+def get_model(num_classes=62, input_shape=(...)):
+    # TODO: add model architecture here
+    # TODO: input shape to be determined @jesper
+    # TODO: should have len(string.ascii_letters + string.digits) classes = 62
+    return None
