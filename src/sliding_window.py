3805375eba5097be1ad1db2f59ff4a6fa06d5087
jesper-sk <47450353+JesperK123456@users.noreply.github.com>
2022-06-04 23:19:50 +0200

    add sliding window fixes

--- src/sliding_window.py
+++ src/sliding_window.py
@@ -22,15 +22,34 @@ class SlidingWindowClassifier:
 
         image_width = resized_im.shape[1]
         window_width = self.window_size[1]
+
+        if new_width < window_width:
+            if len(image.shape) == 3:
+                new_resized = np.zeros((*self.window_size, 3), dtype=np.uint8)
+            else:
+                new_resized = np.zeros(self.window_size, dtype=np.uint8)
+            offset = (window_width - new_width) // 2
+            new_resized[:, offset:offset+new_width, ...] = resized_im
+            resized_im = new_resized
+            new_width = window_width
+            image_width = window_width
+
+        # print('jochie', image_width, window_width, self.hop_size, resized_im.shape, ratio, new_width, image.shape)
         num_steps, rem = divmod((image_width - window_width), self.hop_size)
         slices = []
-        for i in range(0, num_steps-rem):
-            offset = i * self.hop_size
-            slices.append(np.s_[:, offset:offset + window_width, ...])
-        for i in range(num_steps-rem, num_steps):
-            offset = (i + 1) * self.hop_size
-            slices.append(np.s_[:, offset:offset + window_width, ...])
-            assert offset+window_width <= image_width
+        if num_steps-rem > 0:
+            for i in range(0, num_steps-rem):
+                offset = i * self.hop_size
+                # print('resi', offset, window_width)
+                slices.append(np.s_[:, offset:offset + window_width, ...])
+            for i in range(num_steps-rem, num_steps):
+                offset = (i * self.hop_size) + 1
+                # print('resi2', offset, window_width, i, num_steps, rem)
+                slices.append(np.s_[:, offset:offset + window_width, ...])
+        else:
+            slices.append(np.s_[:, 0:window_width, ...])
+            if rem > 0:
+                slices.append(np.s_[:, rem:window_width+rem, ...])
         # Reverse slices to account for RTL
         if self.right_to_left:
             slices.reverse()
@@ -39,8 +58,13 @@ class SlidingWindowClassifier:
     def infer_probability_vector(self, window: np.ndarray) -> np.ndarray:
         if self.model is None:
             return np.random.rand(self.num_classes)
-        window = np.expand_dims(window, axis=(0,3))
+        # print('infer vec', window.shape)
+        if len(window.shape) == 3: # (71, 40, 3) --> (71, 40)
+            window = cv.cvtColor(window, cv.COLOR_RGB2GRAY)
+        window = np.expand_dims(window, axis=(0, 3))
+        # print(window.shape)
         window = np.repeat(window, self.channels, axis=3)
+        # print('infer vec2', window.shape)
         assert window.shape == (1, 71, 40, self.channels)
         predictions = self.model.predict(window)[0]
         # print('Predictions shape:', predictions.shape, ' Predictions: ', predictions)
@@ -50,6 +74,7 @@ class SlidingWindowClassifier:
         resized, slices = self.resize_and_slice(image)
         mat = np.zeros((len(slices), self.num_classes))
         for i, slice in enumerate(slices):
+            # print('infer mat', slice)
             mat[i, :] = self.infer_probability_vector(resized[slice])
         return mat
 
