3f1b9eef6df70de771d68495daa463a270151932
Leander-van-Boven <33727726+Leander-van-Boven@users.noreply.github.com>
2022-06-04 14:12:14 +0200

    added trained models

--- src/dss/model_architecture.py
+++ src/dss/model_architecture.py
@@ -1,33 +1,63 @@
 from tensorflow.python.keras.losses import CategoricalCrossentropy
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, PReLU, Softmax
-
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, PReLU, Softmax, Dropout
+from keras.layers import BatchNormalization
 
 # 5 epochs accuracy 91%
 # 10 epochs same
 # new data only at 90%
-def get_model(num_classes=28, input_shape=(71, 71, 3)):
-    model = Sequential()
-
-    model.add(Conv2D(filters=96, kernel_size=(3, 3), activation=PReLU(), input_shape=input_shape))
-    model.add(MaxPooling2D((3, 3), strides=2))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), activation=PReLU()))
-    model.add(MaxPooling2D((3, 3), strides=2))
-    model.add(Conv2D(filters=160, kernel_size=(3, 3), activation=PReLU()))
-    model.add(MaxPooling2D((3, 3), strides=2))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), activation=PReLU()))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), activation=PReLU()))
-    model.add(Flatten())
-    model.add(Dense(1024, activation=PReLU()))
-    # output layer
-    output_layer = Dense(num_classes, activation=Softmax())
-    model.add(output_layer)
-    # model.summary()
-    return model
-
-
-# def get_model(num_classes=28, input_shape=(71, 40, 3)):
+# def get_model(num_classes=28, input_shape=(71, 71, 3)):
 #     model = Sequential()
+#
+#     model.add(Conv2D(filters=96, kernel_size=(3, 3), activation=PReLU(), input_shape=input_shape))
+#     model.add(MaxPooling2D((3, 3), strides=2))
+#     model.add(Conv2D(filters=128, kernel_size=(3, 3), activation=PReLU()))
+#     model.add(MaxPooling2D((3, 3), strides=2))
+#     model.add(Conv2D(filters=160, kernel_size=(3, 3), activation=PReLU()))
+#     model.add(MaxPooling2D((3, 3), strides=2))
+#     model.add(Conv2D(filters=256, kernel_size=(3, 3), activation=PReLU()))
+#     model.add(Conv2D(filters=256, kernel_size=(3, 3), activation=PReLU()))
+#     model.add(Flatten())
+#     model.add(Dense(1024, activation=PReLU()))
+#     # output layer
+#     output_layer = Dense(num_classes, activation=Softmax())
+#     model.add(output_layer)
+#     # model.summary()
+#     return model
+
+
+def get_model(num_classes=28, input_shape=(71, 40, 3), verbose: bool = False):
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=2, activation=PReLU(), input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=32, kernel_size=3, activation=PReLU()))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=32, kernel_size=5, strides=2, padding='same', activation=PReLU()))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(filters=64, kernel_size=2, activation=PReLU()))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=64, kernel_size=3, activation=PReLU()))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=64, kernel_size=5, strides=2, padding='same', activation=PReLU()))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+
+    model.add(Conv2D(filters=128, kernel_size=3, activation=PReLU()))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=128, kernel_size=3, activation=PReLU()))
+    model.add(BatchNormalization())
+    model.add(Flatten())
+    model.add(Dropout(0.4))
+    model.add(Dense(48, activation=PReLU()))
+    model.add(Dropout(0.4))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    if verbose:
+        print(model.summary())
+
+    return model
 
 
 def compile_model(model):
@@ -39,5 +69,5 @@ def compile_model(model):
 
 
 if __name__ == '__main__':
-    model = get_model()
+    model = get_model(verbose=True)
     # compile_model(model)
